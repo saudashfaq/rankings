b8b835e3573fe3d21535e6f7460cc61f613dbaa7
Arsal <arsalkamoka786@gmail.com>
2021-10-26 02:40:59 -0700

    rearrange file

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,30 +1,22 @@
 
 @extends('layouts.main_layout')
 @section('title') {{'Dashboard'}} @endsection
-<div class="wrapper">
-
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-    </div>
-
-    <!-- Navbar -->
-    <x-app-layout>
-
-        @include('layouts.nav-bar')
-        @include('layouts.contentheader')
-        @include('layouts.sidebar')
-        @include('layouts.content')
-        @include('layouts.footer')
 
 
-    </x-app-layout>
 
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+@section('page-title', 'Dashboard')
 
+@section('bread-crumb')
+
+    <li class="breadcrumb-item"><a href="redirects">Home</a></li>
+    <li class="breadcrumb-item active">Dashboard</li>
+
+@endsection
+
+@section('section-title', 'Dashboard')
+
+@section('content')
+
+    {{--@livewire('users')--}}
+
+@endsection
--- resources/views/campaigns/campaign.blade.php
+++ resources/views/campaigns/campaign.blade.php
@@ -1,81 +1,21 @@
+
 @extends('layouts.main_layout')
 
 @section('title') {{'campaigns'}} @endsection
-{{--@section('title', 'campaigns')--}}
-<div class="wrapper">
 
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-    </div>
+@section('page-title', 'Campaigns Management')
 
-    <!-- Navbar -->
-    <x-app-layout>
+@section('bread-crumb')
 
-        @include('layouts.nav-bar')
+    <li class="breadcrumb-item"><a href="redirects">Home</a></li>
+    <li class="breadcrumb-item active">Campaigns</li>
 
-        @include('layouts.sidebar')
+@endsection
 
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
+@section('section-title', 'Campaign')
 
-                            <h1>Campaigns</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="redirects">Home</a></li>
-                                <li class="breadcrumb-item active">Campaigns</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div><!-- /.container-fluid -->
-            </section>
+@section('content')
 
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card card-primary card-outline">
-                                <div class="card-header">
-                                    <h3 class="card-title">Campaigns</h3>
+    @livewire('campaign-table')
 
-                                </div>
-
-                            @include('livewire.campaigns.create_campaign')
-
-                            <!-- /.card-body -->
-                                <div class="card-footer clearfix">
-                                    <ul class="pagination pagination-sm m-0 float-right">
-                                        <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- /.card -->
-
-
-        </div>
-        @include('layouts.footer')
-
-
-    </x-app-layout>
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+@endsection
