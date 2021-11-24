6c585b8ee06b0b706893d6d26ac63244be06de9a
Arsal <arsalkamoka786@gmail.com>
2021-11-24 14:58:44 +0500

    add logut button

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -67,6 +67,17 @@
 
                         </p>
                     </a>
+                </li>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <div class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        this.closest('form').submit(); " role="button">
+{{--                            <i class="fas fa-sign-out-alt"></i>--}}
+                            {{ __('Log Out') }}
+                        </a>
+                    </div>
+                </form>
             </ul>
 
         </nav>
